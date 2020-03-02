5e78529c42f50cb7d34f053240145beb25d0a4db
Lam Mallari <lamsmallarijr@gmail.com>
2020-03-02 19:39:47 +0800

    redesign action buttons

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import PESO from "../helpers/toPesoFormat";
 
 import { makeStyles } from "@material-ui/core/styles";
@@ -7,27 +7,17 @@ import ListItemText from "@material-ui/core/ListItemText";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
 import ImageIcon from "@material-ui/icons/Image";
-import Collapse from "@material-ui/core/Collapse";
-import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 
 import EditIcon from "@material-ui/icons/Edit";
-import DeleteIcon from "@material-ui/icons/Delete";
 import IconButton from "@material-ui/core/IconButton";
 
-const CartItem = ({ item }) => {
-  const [open, setOpen] = useState(false);
-
+const CartItem = ({ item, handleItemQuantity }) => {
   const useStyles = makeStyles(theme => ({
     price: {
       marginRight: 25
-    },
-    nested: {}
+    }
   }));
-
-  const handleShowActions = () => {
-    setOpen(!open);
-  };
 
   const classes = useStyles();
 
@@ -43,37 +33,21 @@ const CartItem = ({ item }) => {
           primary={item.title}
           secondary={
             <>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.price}
-              >
-                {`Price: ${PESO(item.price)}`}
-              </Typography>
+              {`Price: ${PESO(item.price)}`}
+              <br />
               {`Quantity: ${item.quantity}`}
             </>
           }
         />
-        <Box>
-          <IconButton aria-label="add to likes">
+        <Box component="div" className={classes.actions}>
+          <IconButton
+            onClick={() => handleItemQuantity(item)}
+            aria-label="add to likes"
+          >
             <EditIcon />
-          </IconButton>
-          <IconButton aria-label="add to likes">
-            <DeleteIcon />
           </IconButton>
         </Box>
       </ListItem>
-      <Collapse in={open} timeout="auto" unmountOnExit>
-        <Box className={classes.nested}>
-          <Typography
-            component="span"
-            variant="body2"
-            className={classes.price}
-          >
-            {`Price: ${PESO(item.price)}`}
-          </Typography>
-        </Box>
-      </Collapse>
     </>
   );
 };
