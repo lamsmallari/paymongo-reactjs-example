6ac78aeb26c000f0521be512618348a009e93ed9
Lam Mallari <lamsmallarijr@gmail.com>
2020-03-02 15:56:23 +0800

    develop initial cart list

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,42 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import CartItem from "./CartItem";
 
-const Cart = () => {
+const useStyles = makeStyles(theme => ({
+  title: {
+    marginBottom: 25
+  },
+  root: {
+    width: "100%",
+    maxWidth: 600,
+    backgroundColor: theme.palette.background.paper
+  }
+}));
+
+const Cart = ({ items }) => {
+  const classes = useStyles();
+
   return (
-    <Typography variant="h4" component="h4" gutterBottom>
-      Cart
-    </Typography>
+    <>
+      <Typography
+        variant="h4"
+        component="h4"
+        gutterBottom
+        className={classes.title}
+      >
+        Cart
+      </Typography>
+
+      {items.length > 0 && (
+        <List component="nav" className={classes.root}>
+          {items.map(item => (
+            <CartItem key={item.id} item={item} />
+          ))}
+        </List>
+      )}
+    </>
   );
 };
 
